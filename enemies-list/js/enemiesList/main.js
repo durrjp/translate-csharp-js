1f900ffc06beab0898f69ddf531789f085d953ca
Jon <durrjp1@gmail.com>
2020-05-22 16:49:24 -0500

    started enemies list

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,6 +1,16 @@
 function main() {
   // Put your code here
-
+  const enemies = [
+    {
+      FirstName: "Joshua",
+      LastName: "Flowers",
+      Offenses:[
+        "Being a jerk to me in elementary school",
+        "Not being nice to me in elementary school"
+      ],
+      IsReallyHated: true
+    }
+  ]
 }
 
 main();
