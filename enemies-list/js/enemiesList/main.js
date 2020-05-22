bf7c1c3ba0bcfce9f39dfe4f230cf8e2c0b80f97
Jon <durrjp1@gmail.com>
2020-05-22 18:47:58 -0500

    enemies list complete

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -9,8 +9,49 @@ function main() {
         "Not being nice to me in elementary school"
       ],
       IsReallyHated: true
+    },
+    {
+      FirstName: "Darth",
+      LastName: "Vader",
+      Offenses: [
+          "Cut off Luke's hand",
+          "Murdered all those kids",
+          "Unkind management practices"
+      ],
+      IsReallyHated: false
+    },
+    {
+      FirstName: "Betty",
+      LastName: "Rudelady",
+      Offenses: [
+          "Phone calls in the theater",
+          "Phone calls on the bus",
+          "Phone calls in line at the grocery store",
+          "Poor conversationalist"
+      ],
+      IsReallyHated: true
+    },
+    {
+      FirstName: "Leon",
+      LastName: "Peck",
+      Offenses: [
+          "Keeps giving me a hotplate"
+      ],
+      IsReallyHated: false
     }
   ]
+  console.log("My Enemies List!");
+  console.log("----------------");
+
+  enemies.forEach(e => {
+    if(e.IsReallyHated) {
+      console.log(`${e.FirstName} ${e.LastName} (Really, really dislike!)`)
+    } else {
+      console.log(`${e.FirstName} ${e.LastName}`)
+    }
+    
+  });
+
 }
 
 main();
