2b7ac2835765de184154c054c320c4d0c7c78c63
Jon <durrjp1@gmail.com>
2020-05-22 16:35:52 -0500

    place names completed

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,18 @@
 function main() {
   // Put your code here
+  const placesArray = ["Nashville", "Hong Kong", "The back yard", "Earth",
+  "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
 
+  placesArray.forEach(place => {
+    console.log(place)
+  });
+
+  console.log("")
+
+  const filteredPlaces = placesArray.filter(place => place.startsWith("The"))
+  filteredPlaces.forEach(place => {
+    console.log(place)
+  })
 }
 
 main();
