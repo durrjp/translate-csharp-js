230a230f7fd09bb36d873eb23448230c4dd1986c
Jon <durrjp1@gmail.com>
2020-05-26 15:52:17 -0500

    started, did not finisher insulter exc.

--- insulter/js/insulter/main.js
+++ insulter/js/insulter/main.js
@@ -2,7 +2,7 @@ function main() {
   const insults = [
     "You look like what morning breath smells like.",
     "If you tried to give me cpr I would probably throw myself back under water",
-    "I am not a fan of you",
+      "I am not a fan of you",
     "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
     "I'd rather walk than be on the same plane as you",
     "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
